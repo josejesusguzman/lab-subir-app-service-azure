bf1713025cce681d9aa532519d1766cb88a1cf6b
Jose Jesus Guzman Eusebio <jguzman@innovaccion.mx>
2021-08-25 18:40:44 -0500

    Update app.js

--- api-nodejs/app.js
+++ api-nodejs/app.js
@@ -3,28 +3,25 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 8080
 
 // Para poder obtener información del request de la petición post
 app.use(express.urlencoded({
     extended: true
-  }))
+}))
 
 // Ponemos la funcionalidad de una petición GET
 app.get('/', (req, res) => {
-  res.send('Hola Mundo');
+    res.send('Hola Mundo');
 });
 
 // Ponemos la funcionalidad de una petición POST
 app.post('/', (req, res) => {
   // Regresa el valor de la variable que se recibe con nombre name
-  res.send(req.body.name);
+    res.send(req.body.name);
 });
 
 // Iniciamos el servidor en el puerto 3000
-app.listen(3000, () => {
-  console.log('El servidor se está ejecutando en http://localhost:3000/');
+app.listen(port, () => {
+    console.log('El servidor se está ejecutando en el puerto' + port);
 });
-
